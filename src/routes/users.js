a0e7b29b1edb0b846dc36b50343123a3dfb9fedb
Brendan Boyle <brendancboyle@me.com>
2021-09-22 10:45:12 -0400

    Add cache test

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  if (req.query?.user % 2 === 0) {
+    res.set('Cache-Control', 'public, max-age=304800, immutable')
+  } else if (!req.query?.user) (
+    res.set('Cache-Control', 'public, max-age=604800, immutable')
+  )
   res.send('respond with a resource');
 });
 
